7ed47c42f96f1892f4c85fa05c5a8084d5ca988d
Ko Ko Maung <54410761+koko-maung@users.noreply.github.com>
2019-09-19 22:16:27 -0400

    Create prog6_functions.h

---
+++ prog6_functions.h
@@ -0,0 +1,15 @@
+//Name: Ko Ko Maung
+/*
+ECE Application Programming
+Professor M.Geiger
+4/4/2018
+*/
+#ifndef prog6_functions_h
+#define prog6_functions_h
+
+void ReadText(int histo[], int* max);					//Reads single line of text and updates histogram
+
+void DrawHistogram(int histo[], int max);				//Prints histogram by using array holding frequency values
+
+
+#endif#
